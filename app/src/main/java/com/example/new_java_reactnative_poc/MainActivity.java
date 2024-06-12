e0a0813f66cc933c5ee10c317ba014c86df8bc4e
rahul <rahul.kumawat@tothenew.com>
2024-06-12 17:33:06 +0530

    New Project Added

---
+++ app/src/main/java/com/example/new_java_reactnative_poc/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.new_java_reactnative_poc;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initially load Screen1Fragment
+        if (savedInstanceState == null) {
+            loadFragment(new Screen1Fragment(), false);
+        }
+    }
+
+    public void loadFragment(Fragment fragment, boolean addToBackStack) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        if (addToBackStack) {
+            transaction.addToBackStack(null);
+        }
+        transaction.commit();
+    }
+}
